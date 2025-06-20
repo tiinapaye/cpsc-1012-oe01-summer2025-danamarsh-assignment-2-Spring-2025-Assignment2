75bc24c65c62dc2207945d74cad2802a9d7620a4
tiinapaye <williamcendana0320@gmail.com>
2025-06-20 17:47:20 -0600

    Final Draft
    -not able to add feedback loop in case of user error
    -was not able to prompt the user after the total was calculated
    -overall disappointed with this build

--- ShawarmaCalculator/Program.cs
+++ ShawarmaCalculator/Program.cs
@@ -5,7 +5,10 @@
  * and have the program calculate their order
  * 
  * Author: William Cendana
- * Date: June 17, 2025
+ * Date: June 20, 2025
+ * 
+ * Input: User would enter values that align with their order. If user misinputs any value, program would continue running and create a loop
+ * Output: Program will calculate the total cost of the donair and would prompt the user to try again if chosen
  * 
  * <summary>
  */
@@ -18,28 +21,78 @@ namespace ShawarmaCalculator
         static void Main(string[] args)
         {
             Console.Write("Hello! Welcome to Dilly's Donair, home of the good donair," +
-                " can I take your order?");
+                "can I take your order? ");
 
-            // variables
-            // string(?) = donairSize,extras, addOnItem
-            // int = quantity
-            // double = addOnsprice, discountedSubTotal, subTotal, totalCost 
-            // "that one variable that stays the same" = gst, discount
+            Console.WriteLine();
+            Console.Write("Please choose and write donair size: Regular, Large, Super: ");
+            string input = Console.ReadLine().ToLower();
 
-            // pseudocode portion:
-            // User inputs preferred donair size
-            // -> then prompts addOnItem cheese or meat in seperate lines
-            // -> then prompts quantity - value should be calculated here then assign to a variable
-            // --> each option should have an associated price; when clicked program will add the price to
-            // formula
-            // |--> incorrect input forces back to last prompt (dunno how to do that for now)
-            // Then, program should calculate base donair cost, add-ons, then subtotal
-            // If subtotal is > 40 CAD program adds discount, then calculate subtotal with discount
-            // Apply GST then calculate the total cost
-            // Display result 
-            // Ask if they want to order again
-            
-          
+            const double GST = 0.05,
+                         DISCOUNT = 0.10;
+
+            double donairPrice,
+                   donairSubPrice,
+                   rawValue,
+                   quantity,
+                   addOn,
+                   discountedPrice,
+                   normalTotal,
+                   sum;
+
+            // this is where the user inputs their size of shawarma
+            switch (input)
+            {
+                case "regular":
+
+                    donairPrice = 6.50;
+                    break;
+                case "large":
+
+                    donairPrice = 8.00;
+                    break;
+                case "super":
+
+                    donairPrice = 9.50;
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice. Please restart the program and choose a valid option.");
+                    return;
+            }
+
+            // adding add-ons for the order
+            Console.Write("Would you like to add cheese or meat? Please type cheese / meat / none: ");
+            string secondInput = Console.ReadLine().ToLower();
+            switch (secondInput)
+            {
+                case "cheese":
+                    addOn = 1.25;
+                    break;
+                case "meat":
+                    addOn = 2.00;
+                    break;
+                case "none":
+                    addOn = 0;
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice. Please restart the program and choose a valid option.");
+                    return;
+            }
+
+            Console.Write("How many do you want to order?: ");
+            quantity = int.Parse(Console.ReadLine());
+
+            rawValue = donairPrice + addOn;
+            donairSubPrice = rawValue * quantity;
+
+            // calculating the final result 
+
+            normalTotal = donairSubPrice * GST;
+            sum = normalTotal + donairSubPrice;
+
+            Console.WriteLine($"Your total is {sum}");
+
+
+
         }
     }
 }
